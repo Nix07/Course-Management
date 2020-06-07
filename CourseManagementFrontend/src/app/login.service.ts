3a295f2d5a214e1dee1eb9be2f9e192f990e199f
nix07 <nikhil07prakash@gmail.com>
2020-06-07 20:26:39 +0530

    Resolved minor errors

---
+++ CourseManagementFrontend/src/app/login.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, SystemJsNgModuleLoader } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loggedInFlag = new BehaviorSubject<boolean>(true);
+  currentFlag = this.loggedInFlag.asObservable();
+
+  email: any;
+  password: any;
+  
+  constructor(private http: HttpClient) { }
+
+  changeValue(value: boolean){
+    this.loggedInFlag.next(value);
+  }
+
+  checkCredentials(email, password) {
+    const URL = "http://localhost:8080/login";
+    return this.http.post(URL, {name: '', email: email, password: password} , {responseType: 'text'}); 
+  }
+}
